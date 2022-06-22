221e361e199805536f9c7cd59234fbf296c02897
Jono <jono.forbes@gmail.com>
2022-06-21 18:05:14 -0700

    CameraFollow.cs: use Kinect SDK approach instead of doing a Find("Head")

--- XboxKinect+PerspectiveProjection/Assets/Scriots/CameraFollow.cs
+++ XboxKinect+PerspectiveProjection/Assets/Scriots/CameraFollow.cs
@@ -1,11 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using Windows.Kinect;
 using UnityEngine;
+using Joint = Windows.Kinect.Joint;
 
 public class CameraFollow : MonoBehaviour
 {
-    private Transform _kinectHeadTransform;
+    private Joint _kinectHeadJoint;
     private Vector3 _target;
+
+    [Header("Scene References")]
+    [SerializeField]
+    BodySourceManager m_BodySourceManager;
 
     [Header("Camera Lerp Speed:")]
     [SerializeField]private float lerpSpeed;
@@ -18,43 +24,51 @@ public class CameraFollow : MonoBehaviour
     [SerializeField]private bool isSideView;
     [SerializeField]private Vector3 sideOffset;
     [SerializeField]private Vector3 sideScale;
-
-
-
-    // Update is called once per frame
+    
     void Update()
     {
-        if (_kinectHeadTransform == null) {
-            getData();
-            return;
+        if (_kinectHeadJoint == default)
+        {
+            if (!TryGetHeadJoint())
+            {
+                return;
+            }
         }
 
+        var headPos = _kinectHeadJoint.Position;
         if (!isSideView)
         {
-            _target.x = (_kinectHeadTransform.position.x + offset.x) * scale.x;
-            _target.y = (_kinectHeadTransform.position.y + offset.y) * scale.y;
-            _target.z = (_kinectHeadTransform.position.z + offset.z) * scale.z;
+            _target.x = (headPos.X + offset.x) * scale.x;
+            _target.y = (headPos.Y + offset.y) * scale.y;
+            _target.z = (headPos.Z + offset.z) * scale.z;
         }
         else
         {
-            _target.x = (_kinectHeadTransform.position.z + sideOffset.z) * sideScale.z;
-            _target.y = (_kinectHeadTransform.position.y + sideOffset.y) * sideScale.y;
-            _target.z = (_kinectHeadTransform.position.x + sideOffset.x) * sideScale.x;
+            _target.x = (headPos.X + sideOffset.z) * sideScale.z;
+            _target.y = (headPos.Y + sideOffset.y) * sideScale.y;
+            _target.z = (headPos.Z + sideOffset.x) * sideScale.x;
         }
 
         transform.position = Vector3.Lerp(transform.position, _target, Time.deltaTime * lerpSpeed);
     }
-
-    void getData()
+    
+    bool TryGetHeadJoint()
     {
-        try
-        {
-            _kinectHeadTransform = GameObject.Find("Head").GetComponent<Transform>();
-        }
-        catch
-        {
-            Debug.Log("failed to find person in scene");
-        }
-    } 
-}
+        var data = m_BodySourceManager.GetData();
+        if (data == null)
+            return false;
 
+        Joint joint = default;
+        foreach (var body in data)
+        {
+            if (body.Joints.TryGetValue(JointType.Head, out joint))
+                break;
+        }
+
+        if (joint == default)
+            return false;
+        
+        _kinectHeadJoint = joint;
+        return true;
+    }
+}
